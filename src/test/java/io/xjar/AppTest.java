364750f1ac95fac7448fff0ffc2dcf8c29b3a77b
liubeicai <liubeicai@linklogis.com>
2019-07-10 11:58:02 +0800

    增加测试用例

---
+++ src/test/java/io/xjar/AppTest.java
@@ -0,0 +1,18 @@
+package io.xjar;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+/**
+ * @author liubeicai
+ * @Type AppTest
+ * @date 2019/7/10 11:48
+ * @Version 1.0
+ */
+public class AppTest {
+    @Test
+    public void test(){
+        System.out.println(new Random().nextInt(100));
+    }
+}
